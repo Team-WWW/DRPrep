7fd62142b19a924d83e8014f1bcf1d8f02054395
thorn79 <thorn79@gmail.com>
2013-09-24 21:51:36 -0400

    Splash Screen
    Successfully created splash.xml and splash.java to load splash screen on
    startup

--- src/com/example/android/navigationdrawerexample/Splash.java
+++ src/com/example/android/navigationdrawerexample/Splash.java
@@ -1,9 +1,56 @@
 package com.example.android.navigationdrawerexample;
 
-import android.app.Activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.Window;
 
 public class Splash extends Activity {
+    /**
+     * The thread to process splash screen events
+     */
+    private Thread mSplashThread;
 
-	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splash);
+
+        final Splash sPlashScreen = this;
+
+        mSplashThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (this) {
+                        wait(5000);
+                    }
+                } catch (InterruptedException ex) {
+                }
+
+                finish();
+                Intent intent = new Intent();
+                intent.setClass(sPlashScreen, MainActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        mSplashThread.start();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent evt) {
+        if (evt.getAction() == MotionEvent.ACTION_DOWN) {
+            synchronized (mSplashThread) {
+                mSplashThread.notifyAll();
+            }
+        }
+        return true;
+    }
 }
+
+	    
