628a08ce40f9f9bd0c6969700b7a3cd0ce175663
thorn79 <thorn79@gmail.com>
2013-10-11 17:04:11 -0400

    Scroll/Pictures
    Fixed Scroll layout on all activities and added proper picture to each
    page

--- src/com/example/android/navigationdrawerexample/HomeFragment.java
+++ src/com/example/android/navigationdrawerexample/HomeFragment.java
@@ -1,14 +1,20 @@
 package com.example.android.navigationdrawerexample;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 
-public class HomeFragment extends Fragment
+public class HomeFragment extends Fragment implements OnClickListener
 {
+	
+	Button btnDisaster_Kit;
+	
     public HomeFragment()
     {
 
@@ -21,6 +27,20 @@ public class HomeFragment extends Fragment
 
         getActivity().setTitle("Home!");
         
+        Button dResourceKit = (Button) rootView.findViewById(R.id.btnDisaster_Kit);
+        dResourceKit.setOnClickListener(this);
+        
         return rootView;
+    }
+    
+    @Override
+    public void onClick(View rootView) {
+        switch (rootView.getId()) {
+        case R.id.btnDisaster_Kit:
+        	Intent a = new Intent(getActivity(), DisasterResourceKit.class);
+        	startActivity(a);
+            break;
+        
+        }
     }
 }
