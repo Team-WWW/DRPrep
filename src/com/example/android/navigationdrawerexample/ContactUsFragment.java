d798a1082c4383a4ae06eb7c62ec86e269b7cf10
devryscott <devryscott@gmail.com>
2013-10-12 04:45:09 -0400

    HUGE UPDATE! contact email working plus other misc
    Final update for tonight/this morning. I got an email button on the
    contact page  working, all strings linked up and updated, home page
    changes, and other misc tweaks!

--- src/com/example/android/navigationdrawerexample/ContactUsFragment.java
+++ src/com/example/android/navigationdrawerexample/ContactUsFragment.java
@@ -1,26 +1,58 @@
 package com.example.android.navigationdrawerexample;
 
+
+
 import android.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.text.Html;
 
 
-public class ContactUsFragment extends Fragment
+public class ContactUsFragment extends Fragment implements OnClickListener
 {
-    public ContactUsFragment()
-    {
+   
 
-    }
-
-    @Override
+  @Override
     public View onCreateView(LayoutInflater inflator, ViewGroup container, Bundle savedInstanceState)
     {
-        View rootView = inflator.inflate(R.layout.fragment_contact_us, container, false);
+      View rootView = inflator.inflate(R.layout.fragment_contact_us, container, false);
 
-        getActivity().setTitle("Contact Us");
+       getActivity().setTitle("Contact Us");
+       
+       Button button;
+       
+       button = (Button) rootView.findViewById(R.id.button1);
+       button.setOnClickListener(this);
+            
         
-        return rootView;
-    }
+       return rootView;
+   }
+
+
+
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId())
+		{
+		case R.id.button1:
+			Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+			String[] recipients = new String[]{"devryscott@gmail.com", "thorn79@gmail.com",};
+			emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, recipients);
+			emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "DR. Prep Feedback");
+			emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Please type your comment or question below: \n\n\n\n");
+			emailIntent.setType("text/plain");
+			startActivity(Intent.createChooser(emailIntent, "Send Email with..."));
+			//finish();
+			break;
+		
+
+		}
+	}
 }
